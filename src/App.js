a6d224fbe0e3e93a62772d421ad271c82385ea14
nkchambers <nkchames@gmail.com>
2022-01-17 20:15:00 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import BoxDisplay from './components/BoxDisplay';
+import React, {useState} from 'react';
 
 function App() {
+
+  
+
+
+  /*//ADD BOX FUNCTION
+  const addBox = (newBox) => {
+    console.log(newBox);
+  }*/
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Box Form</h1>
+
+      <hr />
+
+      <BoxDisplay/>
+
     </div>
+
   );
 }
 
---
+++ src/components/BoxDisplay.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const BoxDisplay = (props) => {
+    
+    //STATE VARIABLES
+    console.log(props);
+
+    return (
+        <fieldset>
+            <legend>DisplayBoxes.jsx</legend>
+
+            <hr />
+
+            
+
+
+        </fieldset>
+    )
+}
+
+export default BoxDisplay
